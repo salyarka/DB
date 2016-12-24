b6d767e75073782c0974ce3947f4305873b88acf
Vladimir <salyarka@gmail.com>
2016-12-24 19:34:06 +0300

    add map

--- coordinates/index.php
+++ coordinates/index.php
@@ -4,6 +4,7 @@ require 'yandex_geo/vendor/autoload.php';
 if (isset($_GET['address'])) {
     $api = new \Yandex\Geo\Api();
     $api
+        ->setLimit(1)
         ->setQuery($_GET['address'])
         ->load();
     $response = $api->getResponse();
